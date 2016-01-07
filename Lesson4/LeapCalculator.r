9954f6fdca333ffbe8cc87d327649ed2665b96aa
Dainius <dainius@chiefriver.site>
2016-01-07 11:34:48 +0100

    Addition of more features to the leap year calculator
    It can now handle passing date() as an argument

--- Lesson4/LeapCalculator.r
+++ Lesson4/LeapCalculator.r
@@ -6,11 +6,13 @@
 source("src/leap.r")
 
 # Usage examples / unit tests
-is.leap(2000) # TRUE
-is.leap(2000.8) # TRUE
+is.leap(2016) # TRUE
+is.leap(2000.8) # TRUE: assuming only the integer is wanted
 is.leap(1581) # FALSE with a warning
 is.leap(2002) # FALSE
 is.leap('john') # Error
 is.leap(TRUE) # Error
-is.leap("2000") # TRUE
-is.leap("2000.8") # TRUE
+is.leap(NA) # Error
+is.leap("2000") # TRUE: coercing strings to integers
+is.leap("2000.8") # TRUE: coercing and only using integers
+is.leap(date()) # TRUE: parsing today's date() to get the year
--- Lesson4/src/leap.r
+++ Lesson4/src/leap.r
@@ -5,15 +5,22 @@
 # Calculates whether the year entered is a leap year or not.
 is.leap = function(year)
 {
+    input = year
+
     # If characters, try coercing into a numeric
     if (is.character(year))
-        year = as.numeric(year)
+        year = suppressWarnings(as.numeric(year))
         
     # Sanity checks
-    if (is.numeric(year) == FALSE)
+    if (!is.numeric(year))
         stop("Error: argument of class numeric expected")
     if (is.na(year))
-        stop("Error: argument cannot be NA")
+    {
+        # If it's NA, then it might be due to the user entering NA or a failed coercion; but it could be a date
+        year = try(as.numeric(strsplit(input, " ")[[1]][6]), TRUE)
+        if (!is.numeric(year) || is.na(year) || class(year) == "try-error")
+            stop("Error: failed to parse input argument")
+    }
     if (year < 1582)
         warning(paste(year, "is out of the valid range"))
     
