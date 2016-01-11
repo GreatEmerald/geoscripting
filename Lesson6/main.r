fed1936c437ac01cdd923bb66e2fa449ece08558
Dainius <dainius@chiefriver.site>
2016-01-11 10:45:24 +0100

    Initial commit for lesson 6
    Downloads, extracts and imports the needed shapefiles

---
+++ Lesson6/main.r
@@ -0,0 +1,18 @@
+# Team Rython, Dainius Masiliunas and Tim Weerman
+# Date: 11 January, 2016
+# Apache License 2.0
+
+# Needed packages
+library(rgdal)
+
+# Download the data
+download.file("http://www.mapcruzin.com/download-shapefile/netherlands-places-shape.zip", method="wget", destfile="data/places.zip")
+download.file("http://www.mapcruzin.com/download-shapefile/netherlands-railways-shape.zip", method="wget", destfile="data/railways.zip")
+# Extract the data
+unzip("data/places.zip", exdir="data")
+unzip("data/railways.zip", exdir="data")
+RailwaysFile = "data/railways.shp"
+PlacesFile = "data/railways.shp"
+
+# Import files
+RailwaysSHP = readOGR(RailwaysFile, layer = ogrListLayers(RailwaysFile))
