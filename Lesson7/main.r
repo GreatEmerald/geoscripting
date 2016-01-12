351732957627d34402a3a8e4a0950384ed06285e
Dainius <dainius@chiefriver.site>
2016-01-12 11:11:05 +0100

    Addition of administrative border data

--- Lesson7/main.r
+++ Lesson7/main.r
@@ -2,14 +2,20 @@
 # January 12, 2016
 # Apache License 2.0
 
+library(raster)
 source("src/GetGreennessIndex.r")
 
-# Get data
-
+# Get NDVI data
 download.file("https://github.com/GeoScripting-WUR/VectorRaster/raw/gh-pages/data/MODIS.zip", destfile="data/modis.zip", method="wget")
 unzip("data/modis.zip", exdir="data")
+ModisData = brick(list.files("data", pattern=glob2rx('*.grd'), full.names=TRUE))
 
+# Sanitise data
+ModisData[ModisData < 0] = NA
+
+# Get administrative data
+NLadm <- getData('GADM', country='NLD', level=2, path="data")
 
 # Find the greenest city
 # Visualise results
-GetGreennessIndex(city, month, method)
+GetGreennessIndex(ModisData, NLadm[NLadm@data$NAME_2 == "Wageningen",], 1)
