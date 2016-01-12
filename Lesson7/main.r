b1fc5e9cfaf80465d72ebfb88e936bfb5397f21d
Dainius <dainius@chiefriver.site>
2016-01-12 10:40:10 +0100

    Data directory

--- Lesson7/main.r
+++ Lesson7/main.r
@@ -9,6 +9,7 @@ source("src/GetGreennessIndex.r")
 download.file("https://github.com/GeoScripting-WUR/VectorRaster/raw/gh-pages/data/MODIS.zip", destfile="data/modis.zip", method="wget")
 unzip("data/modis.zip", exdir="data")
 
+
 # Find the greenest city
 # Visualise results
 GetGreennessIndex(city, month, method)
