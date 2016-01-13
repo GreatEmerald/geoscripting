2dd5ed66584566c803c887342b52525da9c91a75
Dainius <dainius@chiefriver.site>
2016-01-13 16:28:00 +0100

    Load resources more efficiently

--- Lesson8/Main.r
+++ Lesson8/Main.r
@@ -9,20 +9,20 @@ library(raster)
 source("src/RMSE.r")
 
 # Download/load information
-download.file("https://github.com/GeoScripting-WUR/AdvancedRasterAnalysis/raw/gh-pages/data/GewataB1.rda", "data/GewataB1.rda", "wget")
-download.file("https://github.com/GeoScripting-WUR/AdvancedRasterAnalysis/raw/gh-pages/data/GewataB2.rda", "data/GewataB2.rda", "wget")
-download.file("https://github.com/GeoScripting-WUR/AdvancedRasterAnalysis/raw/gh-pages/data/GewataB3.rda", "data/GewataB3.rda", "wget")
-download.file("https://github.com/GeoScripting-WUR/AdvancedRasterAnalysis/raw/gh-pages/data/GewataB4.rda", "data/GewataB4.rda", "wget")
-download.file("https://github.com/GeoScripting-WUR/AdvancedRasterAnalysis/raw/gh-pages/data/GewataB5.rda", "data/GewataB5.rda", "wget")
-download.file("https://github.com/GeoScripting-WUR/AdvancedRasterAnalysis/raw/gh-pages/data/GewataB7.rda", "data/GewataB7.rda", "wget")
-download.file("https://github.com/GeoScripting-WUR/AdvancedRasterAnalysis/raw/gh-pages/data/vcfGewata.rda", "data/vcfGewata.rda", "wget")
-load("data/GewataB1.rda")
-load("data/GewataB2.rda")
-load("data/GewataB3.rda")
-load("data/GewataB4.rda")
-load("data/GewataB5.rda")
-load("data/GewataB7.rda")
-load("data/vcfGewata.rda")
+GetFromWURgit = function(filename)
+{
+    download.file(paste("https://github.com/GeoScripting-WUR/AdvancedRasterAnalysis/raw/gh-pages/data/", filename, sep=""),
+        paste("data/", filename, sep=""), "wget")
+    return(paste("data/", filename, sep=""))
+}
+load(GetFromWURgit("GewataB1.rda"))
+load(GetFromWURgit("GewataB2.rda"))
+load(GetFromWURgit("GewataB3.rda"))
+load(GetFromWURgit("GewataB4.rda"))
+load(GetFromWURgit("GewataB5.rda"))
+load(GetFromWURgit("GewataB7.rda"))
+load(GetFromWURgit("vcfGewata.rda"))
+load(GetFromWURgit("trainingPoly.rda"))
 
 # Produce one or more plots that demonstrate the relationship between the Landsat bands and the VCF tree cover. 
 # What can we conclude from this/these plot(s)?
@@ -80,3 +80,4 @@ ReducedRMSE = RMSE(getValues(ReducedBrick[["VCF"]]), getValues(RedPrediction))
 # Are the differences between the predicted and actual tree cover the same for all of the 3 classes we used for the random forest classfication? 
 # Using the training polygons from the random forest classification, calculate the RMSE separately for each of the classes and compare. 
 # Hint - see ?zonal().
+
