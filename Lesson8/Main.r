8f2ead35dc118f7431f14a63a2868722d8c84f32
Dainius <dainius@chiefriver.site>
2016-01-13 11:36:56 +0100

    Add a download method

--- Lesson8/Main.r
+++ Lesson8/Main.r
@@ -9,6 +9,9 @@
 
 
 # Download/load information
-# load("data/GewataB1.rda")
-# load("data/GewataB5.rda")
-# load("data/GewataB7.rda")
+download.file("https://github.com/GeoScripting-WUR/AdvancedRasterAnalysis/raw/gh-pages/data/GewataB1.rda", "data/GewataB1.rda", "wget")
+download.file("https://github.com/GeoScripting-WUR/AdvancedRasterAnalysis/raw/gh-pages/data/GewataB5.rda", "data/GewataB5.rda", "wget")
+download.file("https://github.com/GeoScripting-WUR/AdvancedRasterAnalysis/raw/gh-pages/data/GewataB7.rda", "data/GewataB7.rda", "wget")
+load("data/GewataB1.rda")
+load("data/GewataB5.rda")
+load("data/GewataB7.rda")
