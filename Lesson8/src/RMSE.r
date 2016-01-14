9614abe778e96f6a353e1dcb8f42c711adc581ac
Dainius <dainius@chiefriver.site>
2016-01-14 11:35:37 +0100

    Finished coding

--- Lesson8/src/RMSE.r
+++ Lesson8/src/RMSE.r
@@ -8,6 +8,29 @@ RMSE = function(truth, prediction)
     return(sqrt(mean((truth-prediction)^2, na.rm=TRUE)))
 }
 
-StratifiedRMSE = function(truth, prediction)
+# Calculate partial RMSE for different zones by generating squared difference rasters.
+GetDifferenceRaster = function(former, latter, filename=paste("data/", names(latter), ".grd", sep=""), force=FALSE)
 {
+    # Figure out a reasonable output name
+    if (filename == "data/layer.grd")
+        warning("Please pass filename or assign meaningful names to input rasters! Else the loaded data may not match expectations.")
+    
+    # Create one or load one
+    if (!file.exists(filename) || force)
+    {
+        return(overlay(former, latter, fun=function(truth, prediction){(truth-prediction)^2},
+            filename=filename))
+    }
+    else
+        return(raster(filename))
+}
+
+# Unfortunately, zonal() just passes a vector of numbers, not a matrix. So we have to calclate RMSE manually from a difference raster.
+StratifiedRMSE = function(truth, prediction, zones, zonenames = "", ...)
+{
+    differenceRaster = GetDifferenceRaster(truth, prediction, ...)
+    zonestats = zonal(differenceRaster, zones, fun="mean")
+    if (length(zonenames) > 1)
+        rownames(zonestats) = zonenames
+    return(zonestats)
 }
