50b616da82b8f3bcecaa364495e111f5743e8044
Dainius <dainius@chiefriver.site>
2016-01-15 15:33:57 +0100

    Merge updates

--- Project/main.r
+++ Project/main.r
@@ -2,4 +2,9 @@
 # Date: January 2016
 # License: Apache License 2.0
 
+library(rgeos)
+library(rgdal)
+
 filelist = read.csv("data/data_url_script_2016-01-15_032836.txt")
+
+datum = readGDAL('NETCDF:"data/test.hdf":Lai_500m')
