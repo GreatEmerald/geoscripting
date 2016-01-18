fd8032c2472e1d6b6322bcc2fd78e842ba70452c
Dainius <dainius@chiefriver.site>
2016-01-18 15:32:41 +0100

    Delete unneeded file

--- Assignment1/yesyesremove.sh
+++
@@ -1,5 +0,0 @@
-echo "in the terminal you can use the following bash commands to easily remove files"
-echo "list all the files starting with map*"
-ls data/map*
-echo "remove all the files starting with map*"
-rm -v data/map*
