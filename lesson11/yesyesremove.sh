4892f75abc6387b72d975ac6d891a1653caa22fd
Dainius <dainius@chiefriver.site>
2016-01-19 14:32:39 +0100

    Remove unneeded file again

--- lesson11/yesyesremove.sh
+++
@@ -1,5 +0,0 @@
-echo "in the terminal you can use the following bash commands to easily remove files"
-echo "list all the files starting with testing*"
-ls data/testing*
-echo "remove all the files starting with testing*"
-rm -v data/testing*
