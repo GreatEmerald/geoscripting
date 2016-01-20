049698d577e23e716d7ac9234f7603c6f5e259f7
TimWeatherman <tim.weerman@wur.nl>
2016-01-20 11:52:35 +0100

    one error away from completion

--- Lesson13/Assignment.py
+++ Lesson13/Assignment.py
@@ -6,28 +6,29 @@
 # Import modules
 import urllib
 import os
-from osgeo import gdal, ogr, osr
-
+from osgeo import gdal
+from osgeo.gdalconst import GA_ReadOnly, GDT_Float32
+import numpy as np
 
 # Change working directory
 os.getcwd()
-os.chdir("/home/tim/geoscripting/Lesson13")
 #os.chdir("Yourworkingdirectory/geoscripting/Lesson13")
+os.chdir("/home/tim/geoscripting/Lesson13")
 
-
-# Download the file
-urllib.urlretrieve("https://www.dropbox.com/s/zb7nrla6fqi1mq4/LC81980242014260-SC20150123044700.tar.gz?dl=1"), "data/Netherlands.tar.gz"
-
+##
+### Download the file
+##urllib.urlretrieve("https://www.dropbox.com/s/zb7nrla6fqi1mq4/LC81980242014260-SC20150123044700.tar.gz?dl=1"), "data/Netherlands.tar.gz"
+##
 
 # Untar the file
 filelist = ["LC81980242014260LGN00_sr_band4.tif", "LC81980242014260LGN00_sr_band5.tif"]
 
-for filename in filelist:
-    os.system("tar -zxvf data/Netherlands.tar.gz -C data " +filename)
+##for filename in filelist:
+##    os.system("tar -zxvf data/Netherlands.tar.gz -C data " +filename)
 
 #
-dataSource4 = gdal.Open(filelist[0], GA_ReadOnly)
-dataSource5 = gdal.Open(filelist[1], GA_ReadOnly)
+dataSource4 = gdal.Open("data/" +filelist[0], GA_ReadOnly)
+dataSource5 = gdal.Open("data/" +filelist[1], GA_ReadOnly)
 
 
 # Save bands 4 & 5
@@ -36,36 +37,38 @@ band5 = dataSource5.GetRasterBand(1).ReadAsArray(0,0,dataSource5.RasterXSize, da
 
 
 # Derive NDWI
-NDWI = ((band4 - band5) / (band4 + band5))
+mask = np.not_equal(band5+band4,0)
 
+# Set np.errstate to avoid warning of invalid values (i.e. NaN values) in the divide 
+with np.errstate(invalid='ignore'):
+    ndwi = np.choose(mask,(-99,(band4-band5)/(band4+band5)))
 
-# Current projection is...
-print "\nInformation about " + NDWI 
-print '\nProjection is: ', dataSource.GetProjection()
+#ndwi = ((band4-band5)/(band4+band5))
 
 
 # Save the image
 driver = gdal.GetDriverByName('GTiff')
-outDataSet=driver.Create('/data/ndwi.tif', dataSource4.RasterXSize, dataSource4.RasterYSize, 1, GDT_Float32)
+outDataSet=driver.Create('data/ndwi.tif', dataSource4.RasterXSize, dataSource4.RasterYSize, 1, GDT_Float32)
 outBand = outDataSet.GetRasterBand(1)
 outBand.WriteArray(ndwi,0,0)
+outBand.SetNoDataValue(-99)
+outBand.FlushCache()
+outDataSet.FlushCache()
+
+# Current projection is...
+print "\nInformation about " + ndwi 
+print '\nProjection is: ', dataSource.GetProjection()
 
 
 # Reprojection
-## lat/long definition
-source = osr.SpatialReference()
-source.ImportFromEPSG(4326)
+os.system("gdalwarp -t_srs 'EPSG:4326' data/ndwi.tif data/ndwi_ll.tif")
 
-# http://spatialreference.org/ref/sr-org/6781/
-# http://spatialreference.org/ref/epsg/28992/
-target = osr.SpatialReference()
-target.ImportFromEPSG(28992)
 
-transform = osr.CoordinateTransformation(source, target)
-point = ogr.CreateGeometryFromWkt("POINT (5.6660 -51.9872)")
-point.Transform(transform)
-print point.ExportToWkt()source = osr.SpatialReference()
-source.ImportFromEPSG(4326)
+# Current projection is...
+reprojected = gdal.Open("data/ndwi_ll.tif", GA_ReadOnly)
+print "\nInformation about the new reprojected file ndwi_ll.tif" 
+print '\nProjection is: ', reprojected.GetProjection()
+
 
 # End of the script
 print "This is the end of the script"
