73635b0d991cf727deba9cdeaa500cb6015fcfaf
TimWeatherman <tim.weerman@wur.nl>
2016-01-25 17:11:47 +0100

    Small what to do list + extended the csv_editor

--- Project/Presentation/csv_editor.r
+++ Project/Presentation/csv_editor.r
@@ -3,7 +3,9 @@
 # License: Apache License 2.0
 
 # Load library
-
+library (lattice)
+library(leaflet)
+library(shiny)
 
 # Set working directory
 getwd()
@@ -18,7 +20,7 @@ names(statistics)[2] = "Year"
 names(statistics)[3] = "Forest_Coverage_in_Percent"
 
 # View the data frame
-View(statistics)
+# View(statistics)
 
 # Remove the information in the bottom
 statistics = statistics[-c(1225:1243),]
@@ -34,5 +36,28 @@ for( i in 1:nrow(statistics))
 
 # Remove all the years that have No values at all (1993, 1998, 2001, 2003, 2015)
 statistics = statistics[!is.na(statistics$Forest_Coverage_in_Percent),]
+
+# Plot all the data
+xyplot(Forest_Coverage_in_Percent~ Year|Municipality, data=statistics)
+
+# Levels
+statistics$Municipality=factor(statistics$Municipality)
+levels(statistics$Municipality)
+
+
+for(i in levels(statistics$Municipality))
+{
+  svg(paste("ToManyImages/", i, ".svg"))
+  plot(Forest_Coverage_in_Percent~ Year, data=statistics[statistics$Municipality==i,], main = i, type = "l", ylim=c(0,100))
+  dev.off()
+}
+
+
+content <- as.character(tagList(tags$img(src="/home/tim/geoscripting/Project/Presentation/ToManyImages/ Akmenė d. mun. .svg")))
+
+leaflet() %>% addTiles() %>%
+  addPopups(-122.327298, 47.597131, content,
+            options = popupOptions(closeButton = FALSE)
+  )
 
 
