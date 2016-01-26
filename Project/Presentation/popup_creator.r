227e675862a7edf1d187da60750df5633ec8de34
TimWeatherman <tim.weerman@wur.nl>
2016-01-26 16:08:19 +0100

    Popup can be created with the right size, not generic yet

--- Project/Presentation/popup_creator.r
+++ Project/Presentation/popup_creator.r
@@ -11,9 +11,4 @@ m <- leaflet() %>%
   addMarkers(lng=22.826354, lat=56.237323, popup= ("<img src='/home/tim/geoscripting/Project/Presentation/ToManyImages/ Akmenė d. mun. .svg' height='300' width='300'></img>"))
 m  # Print the
 
-
-# popupOptions(maxWidth = NULL) editing the width so it's not 300 by default should be done with this
-m <- leaflet() %>%
-  addTiles() %>%  # Add default OpenStreetMap map tiles
-  addMarkers(lng=22.826354, lat=56.237323, popup= ("<img src='http://www.royaltyfreeimages.net/wp-content/uploads/2010/09/royalty-free-images-mushroom.jpg' height='768' width='1024'></img>"))
-m  # Print the
+###<link rel="stylesheet" type="text/css" href="/home/tim/geoscripting/Project/Presentation/width.css">
