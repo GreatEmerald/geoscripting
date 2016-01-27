f720f0e72941d9eba4665ad03b9143b255d0815b
TimWeatherman <tim.weerman@wur.nl>
2016-01-27 11:35:43 +0100

    Almost done with presenting the plots

--- Project/Presentation/Polygon_popup.r
+++ Project/Presentation/Polygon_popup.r
@@ -6,16 +6,18 @@
 library(raster)
 library(leaflet)
 
-testcase = "<img src='file:///home/tim/geoscripting/Project/Presentation/pre-processing.png' height='400' width='400'></img>"
+
+ImageTagStart = "<img src='file:///home/tim/geoscripting/Project/Presentation/ToManyImages/" 
+ImageTagEnd =".svg' height='450' width='450'></img>"
 leaflet() %>% addProviderTiles("Esri.WorldImagery", group = "Satellite") %>% 
   addPolygons(data = LTU2, fill = FALSE, stroke = TRUE, color = "#03F", group = "Borders") %>% 
   addPolygons(data = LTU2, fill = TRUE, stroke = FALSE, color = "#f93", 
-              popup = paste0(as.character(testcase)), group = "Polygons") %>% 
+              popup = paste0(ImageTagStart, SanitiseNames(LTU2@data$VARNAME_2), ImageTagEnd), group = "Polygons") 
   # add a legend
   #addLegend("bottomright", colors = c("#03F", "#f93"), labels = c("Borders", "Polygons")) %>%   
   # add layers control
-  addLayersControl(position = 'topright',
-    baseGroups = c("Topographical", "Road map", "Satellite"),
+  #addLayersControl(position = 'topright',
+    #baseGroups = c("Topographical", "Road map", "Satellite"),
     #overlayGroups = c("Borders", "Polygons"),
-    options = layersControlOptions(collapsed = FALSE)
-  )
+    #options = layersControlOptions(collapsed = FALSE)
+  #)
