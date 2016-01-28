1bc418cefaa0daa089d81e72fdfc8d075c1500ec
Dainius <pastas4@gmail.com>
2016-01-28 09:11:16 +0100

    Add a summary raster render

--- Project/src/GetMunicipalityLeaflet.r
+++ Project/src/GetMunicipalityLeaflet.r
@@ -8,13 +8,14 @@ library(leaflet)
 library(rgeos)
 
 # Returns a Leaflet object consisting of interactive administrative divisions
-# that show pregenerated SVG plots. PolyL0 is the top level administrative polgons,
-# PolyL2 is the municipality level polygons (currently level 2 in GADM).
+# that show pregenerated SVG plots. polyl0 is the top level administrative polgons,
+# polyl2 is the municipality level polygons (currently level 2 in GADM).
+# overlayraster is a raster to display at the base of the plot.
 # svgdir is the location of preset SVGs.
-GetMunicipalityLeaflet = function(PolyL0, PolyL2, svgdir="output/SVG")
+GetMunicipalityLeaflet = function(polyl0, polyl2, overlayraster, rastername="Mean LAI", svgdir="output/SVG")
 {
     # Finding the central point of Lithuania
-    centroid = gCentroid(PolyL0, byid=FALSE, id = NULL)
+    centroid = gCentroid(polyl0, byid=FALSE, id = NULL)
 
     # Variables that hold the information for the polygon popup
     ImageTagStart = paste0("<img src='file://", getwd(), "/", svgdir, "/") 
@@ -22,10 +23,12 @@ GetMunicipalityLeaflet = function(PolyL0, PolyL2, svgdir="output/SVG")
 
     # Leaflet function which creates a map with clickable municipailities. Each municipality has its own graph.
     result = leaflet() %>% addProviderTiles("Esri.WorldImagery", group = "Satellite") %>% 
-    addPolygons(data = PolyL2, fill = FALSE, stroke = TRUE, color = "#03F", group = "Borders") %>% 
-    addPolygons(data = PolyL2, fill = TRUE, stroke = FALSE, color = "#f93", 
-                popup = paste0(ImageTagStart, SanitiseNames(PolyL2@data$VARNAME_2), ImageTagEnd), group = "Polygons") %>%
+    addRasterImage(overlayraster, group=rastername) %>%
+    addPolygons(data = polyl2, fill = FALSE, stroke = TRUE, color = "#03F", group = "Borders") %>% 
+    addPolygons(data = polyl2, fill = TRUE, stroke = FALSE, color = "#f93", 
+                popup = paste0(ImageTagStart, SanitiseNames(polyl2@data$VARNAME_2), ImageTagEnd), group = "Polygons") %>%
     # Central marker for the whole country
-    addMarkers(centroid@coords[1], centroid@coords[2], popup = paste0("<img src='file://", getwd(), "/", svgdir, "/Republic of Lithuania.svg' height='450' width='450'></img>")) 
+    addMarkers(centroid@coords[1], centroid@coords[2], popup = paste0("<img src='file://", getwd(), "/", svgdir, "/Republic of Lithuania.svg' height='450' width='450'></img>")) %>%
+    addLayersControl(overlayGroups = c("Satellite", rastername))
     return(result)
 }
